285fa2448ca6bd5eeabed4cb97d3319558d88946
kendradalley <kendracdalley@gmail.com>
2016-11-17 13:21:08 -0800

    created OMDBSearch component

---
+++ src/components/OMDBSearch.jsx
@@ -0,0 +1,16 @@
+// dependency
+const React = require('react');
+
+// component
+const OMDBSearch = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Search for Movies</h1>
+      </div>
+      );
+  }
+});
+
+// export module
+module.exports = OMDBSearch;
